257ea5325020a8a7fb003828e53146e7c0ad4533
szymon.szpakowski <szymon666>
2019-07-01 11:52:14 +0200

    impl find owner. closes #54

--- pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/OwnerRepository.java
@@ -3,8 +3,13 @@ package com.springframework.petclinic.repositories;
 import com.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
 
 }
