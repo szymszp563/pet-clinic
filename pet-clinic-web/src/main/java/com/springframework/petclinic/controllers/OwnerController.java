df421d945e46852af5aea9b7a22643071efbda7b
szymon.szpakowski <szymon666>
2019-07-01 10:11:49 +0200

    added display for owner record. closes #53

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -4,7 +4,10 @@ import com.springframework.petclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping({"/owners"})
 @Controller
@@ -29,4 +32,14 @@ public class OwnerController {
     public String findOwners(){
         return "notimplemented";
     }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 }
+
+
